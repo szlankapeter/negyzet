1eb2cde17263a582d3ed14ab2d36d6011eb9e325
szlankapeter <szlankapeter@gmail.com>
2023-10-09 13:03:14 +0200

    1. feladat

--- src/negyzet/Negyzet.java
+++ src/negyzet/Negyzet.java
@@ -1,9 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package negyzet;
 
+/**
+ *
+ * @author szlanka.peter
+ */
 public class Negyzet {
+    
+    private int x;
+    private int y;
+    private int oldal;
 
-    public static void main(String[] args) {
-       
+    public void setX(int x) {
+        this.x = x;
     }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setOldal(int oldal) {
+        this.oldal = oldal;
+    }
+    
+    
+    public String allapot(){
+        return "x=" + this.x + " y=" + this.y + " oldal=" + this.oldal;
+    }
+    
+    public int terulet(){
+        return oldal * oldal;
+    }
+    
+    
     
 }
---
+++ src/negyzet/Program.java
@@ -0,0 +1,18 @@
+package negyzet;
+
+public class Program {
+
+    public static void main(String[] args) {
+       feladat();
+    }
+    
+    public static void feladat(){
+        Negyzet a = new Negyzet();
+        a.setOldal(5);
+        a.setX(3);
+        a.setY(4);
+        System.out.println(a.allapot());
+        System.out.println(a.terulet());
+    }
+    
+}
